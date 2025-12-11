ad0dd200e9270f9e853f85b3a96c23f3adad041c
akira21kaji <arkamiyama21@gmail.com>
2025-12-11 09:05:21 +0900

    fetchの課題７まで実施

--- fetch-demo/fetch.js
+++ fetch-demo/fetch.js
@@ -61,4 +61,63 @@
 // }
 // main();
 
+//課題５
+// const lostUrl = "https://jsonplaceholder.typicode.com/thttps://httpstat.us/404";
+
+// async function main() {
+//   try {
+//     const res = await fetch(lostUrl);
+//     if (!res.ok) throw new Error(`HTTP ${res.status} ${res.statusText}`);
+//     const data = await res.json();
+//     console.log(data);
+//   } catch (error) {
+//     console.error("見つかりません");
+//   }
+// }
+// main();
+
 //課題６
+// const api = new URL("https://jsonplaceholder.typicode.com/comments");
+// api.searchParams.set("postId", "2");
+
+// async function main() {
+//   try {
+//     const res = await fetch(api);
+//     if (!res.ok) throw new Error(`HTTP ${res.status}`);
+//     const data = await res.json();
+//     console.log(
+//       "email:",
+//       data.map((m) => m.email)
+//     );
+//   } catch (error) {
+//     console.error("失敗:", error);
+//   }
+// }
+// main();
+
+//課題７
+// const api = "https://jsonplaceholder.typicode.com/posts";
+// async function main() {
+//   try {
+//     const res = await fetch(api, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({
+//         title: "これが",
+//         body: "課題７の中身です",
+//         userId: 1,
+//       }),
+//     });
+//     if (!res.ok) throw new Error(`HTTP ${res.status}`);
+//     const data = await res.json();
+//     console.log(data.id);
+//   } catch (error) {
+//     console.error("失敗:", error);
+//   }
+// }
+// main();
+
+//課題８
+const timeOutUrl = "https://httpstat.us/200?sleep=7000";
